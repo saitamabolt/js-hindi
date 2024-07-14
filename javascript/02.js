edd3da5b5d8e29fb61f0ed0958d7f9d79b0bcbd7
saitamabolt <162406790+saitamabolt@users.noreply.github.com>
2024-07-14 08:29:17 +0000

    dataype

---
+++ javascript/02.js
@@ -0,0 +1,24 @@
+//learning about data types and other important things 
+
+"use strict";  // treat all js code as newer version by default bhi set hota h 
+//alert(3+3) 
+/*
+normally brower inspect pr jake udhr ke console pr chlega
+but nodejs me yaha define karna padta h direct nhi chlega
+
+
+*/
+//datatypes
+
+//numers ==> range 2 to 53
+//bigInt
+//string ==>"",''
+//boolean ==> true/false
+//null ==> standalone value
+//undefined ==>
+//symbol ==>umique
+
+//object
+
+console.log(typeof null);   //object
+console.log(typeof undefined);   //undefined
