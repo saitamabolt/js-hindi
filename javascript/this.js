502efc02c32774b8544a8ef597a850395cfe8072
saitamabolt <162406790+saitamabolt@users.noreply.github.com>
2024-07-27 07:07:56 +0000

    arrow updated

--- javascript/this.js
+++ javascript/this.js
@@ -30,3 +30,43 @@ function billa2() {
 }
 billa2() //umdefined 
 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// // const chai=function () {
+// //     let nam ="bhanuuu";
+// //     console.log(this.nam);
+    
+// // }
+
+// chai() //undefined abhi bhi nhi pata isko never read 
+
+//++++++++++++++++++++++++++++++ in arrow function +++++++++++++++++++++++++++++++++++++++++++
+const chai = () => {
+    let nam ="bhanuuu";
+    console.log(this.nam);
+    
+}
+chai() //still undefined 
+
+//+++++++++++++++++++++++++++++++++++ arrow function ++++++++++++++++++++++++++++++++++++++++
+
+//syntax ()=> {}  or const kuchBhi = ()=> {}
+
+const sum = (value1,value2) => {
+    return value1+value2;
+}    
+console.log(sum(5,6)) //normally sum krdega 
+
+//+++++++++++++++++++++++++++++++++++++++ different way +++++++++++++++++++++++++++++++++++++++++
+//now same thing in different way 
+
+const threeSum = (num1,num2,num3)=> num1+num2+num3;
+console.log(threeSum(1,2,3));
+
+//+++++++++++++++++++++++++ note +++++++++++++++++++++++++
+
+//  {} curloy bracese melihka toh return lihna padega or () iske andr nhi likhna padega 
+
+
+const threeSumm = (num1,num2,num3)=> ( num1+num2+num3 ) //useful in react 
+console.log(threeSumm(1,2,3));
