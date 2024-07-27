1a09d49102150031040bcb8a6492c4631a3805d1
saitamabolt <162406790+saitamabolt@users.noreply.github.com>
2024-07-27 06:40:44 +0000

    updated this

--- javascript/this.js
+++ javascript/this.js
@@ -15,4 +15,18 @@ userName.name= "ravi",
 userName.welcomeMessage() //global curent contect me username ravi bana h
 console.log(this); //empty object aayega  node pr but brower me dekhe toh windows aata h 
 
+// important things for this  used inside function
+
+function bila(){
+    console.log(this);
+}
+bila() //bhut sari values aayengi mtlb this me kuch hota toh h 
+
+//now same thing while define something in function
+function billa2() {
+    let nam ="bhanuuu";
+    console.log(this.nam);
+    
+}
+billa2() //umdefined 
 
