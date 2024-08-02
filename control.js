ae0d21f8772457a831e03daa338fda663bf83d7e
saitamabolt <162406790+saitamabolt@users.noreply.github.com>
2024-08-02 04:40:47 +0000

    truth

---
+++ control.js
@@ -0,0 +1,34 @@
+//control flow 
+//kuch condtion se excute hona chiaye sara nhi chlna chiaye ek sath
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = "march"
+// switch (month) {
+//     case "jan":
+//         console.log("january");    
+//         break;
+//     case "feb":
+//         console.log("february");    
+//         break;
+//     case "march":
+//         console.log("march");    
+//         break;
+//     case 4:
+//         console.log("april");    
+//         break;
+    
+
+//     default:
+//         break;
+// }
+
+
+ 
---
+++ javascript/truth.js
@@ -0,0 +1,49 @@
+//falsy value ya false value 
+
+/* 
+  false 
+  0
+  -0
+  big INt 0n
+  ""
+  null
+  undefined 
+  NAn
+*/
+
+//truthy value 
+
+/*
+
+ "0" string ke aandr 0
+ 'false'
+ " " truth h becoz space di h
+ []
+ {}
+ function(){}
+
+
+
+*/
+
+
+//**************************************** to vheck array is empty *********************************************************** */
+/*
+const arr=[];
+if(arr.length===0){
+    console.log("array is empty");
+}
+console.log(typeof arr) //object
+*/
+
+//**************************************** to vheck object is empty *******************************************
+
+const myObj={}
+if(Object.keys(myObj).length ===0){
+    console.log("object is empty");
+}
+
+
+
+
+                        
