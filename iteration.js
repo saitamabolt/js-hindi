162de9fe3c8102fb002e69e81d7c0d8e47e9dec8
saitamabolt <162406790+saitamabolt@users.noreply.github.com>
2024-08-04 05:26:17 +0000

    loops

---
+++ iteration.js
@@ -0,0 +1,27 @@
+// //for
+
+// const marHero =["spiderman", "thor","ironman"]
+// let arr=0
+
+// while(arr<marHero.length){
+//     console.log(`my hero is ${marHero[arr]}`);
+//     arr =arr+1
+    
+// }
+//for of 
+
+//["","",""]
+
+//[{},{},{}]
+
+// for (const element of object) {     ya object normal wording me h mtlb jo bhi ho 
+    
+// }
+const heroArr=["spiderman", "thor","ironman",1,2,3 ]
+
+for (const hero of heroArr) {
+    console.log(hero);
+    
+    
+}
+
